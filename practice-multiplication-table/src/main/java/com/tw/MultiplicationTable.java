82d74ffee0b2d8786db3e8981bd02f06b0fc3711
Bryan Jay Zacarias <zacarbr@oocl.com>
2022-08-30 15:51:37 +0800

    feat: generating Single Line expression

---
+++ practice-multiplication-table/src/main/java/com/tw/MultiplicationTable.java
@@ -0,0 +1,33 @@
+package com.tw;
+
+import oracle.jrockit.jfr.openmbean.ProducerDescriptorType;
+
+public class MultiplicationTable {
+    public String create(int start, int end) {
+        return null;
+    }
+
+    public Boolean isValid(int start, int end) {
+        return isInRange(start) && isInRange(end) && isStartNotBiggerThanEnd(start, end);
+    }
+
+    public Boolean isInRange(int number) {
+        return number >=1 && number <=1000;
+    }
+
+    public Boolean isStartNotBiggerThanEnd(int start, int end) {
+        return start <= end;
+    }
+
+    public String generateTable(int start, int end) {
+        return null;
+    }
+
+    public String generateLine(int start, int row) {
+        return null;
+    }
+
+    public String generateSingleExpression(int multiplicand, int multiplier) {
+        return  multiplicand + "*" + multiplier + "=" + (multiplicand * multiplier);
+    }
+}
